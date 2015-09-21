ce8a8b668ddc184fd0cd98d46259a63a884edd8b
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2015-09-21 12:01:26 +0200

    add touch test

--- app/components/mapbox-map.js
+++ app/components/mapbox-map.js
@@ -1,6 +1,29 @@
 import Ember from 'ember';
 import MapboxGl from 'ember-cli-mapbox-gl/mixins/mapbox-gl';
+import Gestures from 'ember-cli-tuio/mixins/gestures';
 
-export default Ember.Component.extend(MapboxGl, {
-  classNames: ['mapbox-map']
+export default Ember.Component.extend(MapboxGl, Gestures, {
+  classNames: ['mapbox-map'],
+
+  gestures: ['tap', 'pinch', 'pinchstart'],
+
+  recognizers: {
+    tap: { threshold: 40 },
+    pinch: { enable: true}
+  },
+
+  tap: function() {
+
+  },
+  click: (e) => {
+    console.log(e);
+  },
+
+  touchStart: function(e) {
+    console.log(e.originalEvent.changedTouches[0]);
+  },
+
+  pinch: function() {
+    console.log('pinch');
+  }
 });
