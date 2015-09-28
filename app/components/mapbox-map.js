7cefb5a14346e8914bffcf00ddc9504bf4826fb2
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2015-09-28 13:39:37 +0200

    add map demos

--- app/components/mapbox-map.js
+++ app/components/mapbox-map.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 import MapboxGl from 'ember-cli-mapbox-gl/mixins/mapbox-gl';
 
-export default Ember.Component.extend(MapboxGl, {
+const {
+  Component,
+  bind,
+  run: {
+    later
+  }
+} = Ember;
+
+export default Component.extend(MapboxGl, {
   classNames: ['mapbox-map'],
 
   mapSettings: {
@@ -10,5 +18,51 @@ export default Ember.Component.extend(MapboxGl, {
     lng: 13.404954,
     zoom: 12,
     interactive: true
+  },
+
+  actions: {
+    toggleClass: function() {
+      if ( this.get('map').hasClass('night') ) {
+        this.get('map').removeClass('night');
+        console.log('day');
+      } else {
+        this.get('map').addClass('night');
+        console.log('night');
+      }
+    },
+
+    moveTo: function(target) {
+      if(target === 'ber') {
+        this.get('map').flyTo({
+          center: [13.404954, 52.520007],
+          zoom: 13
+        });
+      }
+
+      if(target === 'muc') {
+        this.get('map').flyTo({
+          center: [11.581981, 48.135125],
+          zoom: 13
+        });
+      }
+
+      if(target === 'muc-easeIn') {
+        this.get('map').easeTo({
+          center: [11.557768, 48.140350],
+          zoom: 16,
+          bearing:60,
+          pitch:55
+        });
+      }
+
+      if(target === 'muc-easeOut') {
+        this.get('map').easeTo({
+          center: [11.581981, 48.135125],
+          zoom: 13,
+          bearing:0,
+          pitch:0
+        });
+      }
+    }
   }
 });
