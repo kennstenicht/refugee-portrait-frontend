19d7cc83518a7f8509c903f669e108331e5904f5
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2015-10-06 16:31:36 +0200

    add story and chapter route

---
+++ tests/unit/routes/story-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:story', 'Unit | Route | story', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
