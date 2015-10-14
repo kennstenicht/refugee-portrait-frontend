de1492e63f339b1c0e806815502e7c3a812eccc0
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2015-10-14 11:18:48 +0200

    include setMood service

--- app/services/targeting.js
+++ app/services/targeting.js
@@ -6,7 +6,28 @@ const {
 } = Ember;
 
 export default Service.extend(Evented, {
+  currentTarget: null,
+
+  feelings: {
+    angst: 'dark',
+    stress: 'dark',
+    frustriert: 'dark',
+    hoffnung: ''
+  },
+
   setTarget: function (chapter) {
-    this.trigger('newTarget', chapter);
+    if(this.get('currentTarget') !== chapter) {
+      this.trigger('newTarget', chapter);
+      this.set('currentTarget', chapter);
+      this.setMood(chapter.feeling);
+    }
+  },
+
+  setMood: function (feeling) {
+    let mood = feelings[feeling];
+    if(this.get('currentMood') !== mood) {
+      this.trigger('newMood', mood);
+      this.set('currentMood', mood);
+    }
   }
 });
