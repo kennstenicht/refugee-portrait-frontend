f6e877703a66c0ea5c75ea37fb97288b9ffecf4c
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2015-10-14 16:34:32 +0200

    close chapter

--- app/controllers/chapter.js
+++ app/controllers/chapter.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  actions: {
+    closeChapter: function () {
+      this.transitionToRoute('story', this.get('model.story'));
+    }
+  }
 });
