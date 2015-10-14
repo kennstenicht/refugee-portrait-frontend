bbab5c69505b82ab984d092149ef30fc0625c270
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2015-10-14 10:52:25 +0200

    use service to get new targets
    use local mapbox style

--- app/components/mapbox-map.js
+++ app/components/mapbox-map.js
@@ -3,6 +3,8 @@ import MapboxGl from 'ember-cli-mapbox-gl/mixins/mapbox-gl';
 
 const {
   Component,
+  on,
+  init,
   observer
 } = Ember;
 
@@ -10,50 +12,23 @@ export default Component.extend(MapboxGl, {
   classNames: ['mapbox-map'],
 
   mapSettings: {
-    style: "mapbox://styles/mapbox/dark-v8",
+    style: "mapbox-style.json",
     lat: 52.498605,
     lng: 13.391799,
     zoom: 12,
     interactive: true
   },
 
-  moveToTarget: observer('target', function() {
-    this.send('moveTo', this.get('target'));
+  targeting: Ember.inject.service('targeting'),
+
+  listen: on('init', function() {
+      this.get('targeting').on('newTarget', this, 'setTarget');
   }),
 
-  actions: {
-    moveTo: function(target) {
-      this.set('target', target);
-
-      if(target === 'ber') {
-        this.get('map').flyTo({
-          center: [13.391799, 52.498605],
-          zoom: 13
-        });
-      }
-
-      if(target === 'muc') {
-        this.get('map').flyTo({
-          center: [11.581981, 48.135125],
-          zoom: 13
-        });
-      }
-
-      if(target === 'easeIn') {
-        this.get('map').easeTo({
-          zoom: 16,
-          bearing:60,
-          pitch:55
-        });
-      }
-
-      if(target === 'easeOut') {
-        this.get('map').easeTo({
-          zoom: 13,
-          bearing:0,
-          pitch:0
-        });
-      }
-    }
+  setTarget: function (chapter) {
+    this.get('map').flyTo({
+      center: [chapter.get('lng'), chapter.get('lat')],
+      zoom: 10 + chapter.get('accuracy')
+    });
   }
 });
