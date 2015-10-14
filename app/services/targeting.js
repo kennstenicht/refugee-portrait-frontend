b961983b550ca890d9f55ff278b014ec30e17065
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2015-10-14 14:42:16 +0200

    fixes

--- app/services/targeting.js
+++ app/services/targeting.js
@@ -9,19 +9,20 @@ export default Service.extend(Evented, {
   feelings: {
     angst: 'dark',
     stress: 'dark',
-    frustriert: 'dark',
+    frustriert: 'dark'
   },
 
   setTarget: function (chapter) {
     if(this.get('currentTarget') !== chapter) {
       this.trigger('newTarget', chapter);
       this.set('currentTarget', chapter);
+      this.trigger('newChapter', chapter);
       this.setMood(chapter.get('feeling'));
     }
   },
 
   setMood: function (feeling) {
-    let mood = this.get('feelings')[feeling] || 'default';
+    let mood = this.get('feelings.'+feeling) || 'default';
     if(this.get('currentMood') !== mood) {
       this.trigger('newMood', mood);
       this.set('currentMood', mood);
