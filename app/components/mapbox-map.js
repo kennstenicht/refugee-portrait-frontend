576740db6e568cc0e47e4c625cbe159a32439397
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2015-10-15 18:35:49 +0200

    draw path on map and roatet map

--- app/components/mapbox-map.js
+++ app/components/mapbox-map.js
@@ -3,6 +3,7 @@ import MapboxGl from 'ember-cli-mapbox-gl/mixins/mapbox-gl';
 
 const {
   Component,
+  computed,
   on,
   init,
   observer
@@ -13,9 +14,10 @@ export default Component.extend(MapboxGl, {
 
   mapSettings: {
     style: "mapbox-style.json",
-    lat: 52.498605,
-    lng: 13.391799,
-    zoom: 12,
+    lat: 44.016521,
+    lng: 21.005859,
+    zoom: 4.5,
+    bearing: -130,
     interactive: true
   },
 
@@ -26,10 +28,69 @@ export default Component.extend(MapboxGl, {
       this.get('targeting').on('newMood', this, 'setMood');
   }),
 
+  didInsertElement: function () {
+    let map = this.get('map');
+
+    map.on('style.load', function () {
+        map.addSource("route", {
+            "type": "geojson",
+            "data": {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": [
+                        [36.284775, 33.493496],
+                        [35.664368, 34.254379],
+                        [34.641481, 36.812104],
+                        [34.641481, 36.812104],
+                        [34.578094, 36.770517],
+                        [34.641481, 36.812104],
+                        [27.142826, 38.423734],
+                        [26.277707, 39.264510],
+                        [25.775299, 38.016452],
+                        [23.729360, 37.983917],
+                        [22.944419, 40.640063],
+                        [22.944419, 40.640063],
+                        [22.494507, 41.140399],
+                        [21.745275, 41.608635],
+                        [21.725694, 42.132289],
+                        [21.967163, 42.312862],
+                        [20.448922, 44.786568],
+                        [19.605103, 46.167468],
+                        [19.605103, 46.167468],
+                        [19.503304, 47.162494],
+                        [16.373819, 48.208174],
+                        [11.581981, 48.135125],
+                        [9.993682, 53.551085],
+                        [12.568337, 55.676097],
+                        [9.792178, 54.913811]
+                    ]
+                }
+            }
+        });
+
+        map.addLayer({
+            "id": "route",
+            "type": "line",
+            "source": "route",
+            "layout": {
+                "line-join": "round",
+                "line-cap": "round"
+            },
+            "paint": {
+                "line-color": "#888",
+                "line-width": 8
+            }
+        });
+    });
+  },
+
   setTarget: function (chapter) {
-    this.get('map').flyTo({
+    this.get('map').easeTo({
       center: [chapter.get('lng'), chapter.get('lat')],
-      zoom: 10 + chapter.get('accuracy')
+      zoom: 10 + chapter.get('accuracy'),
+      duration: 2000
     });
   },
 
