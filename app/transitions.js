a006ccffdc98632546d08b94a109459a3cfc9359
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2015-10-15 18:36:33 +0200

    fixes

--- app/transitions.js
+++ app/transitions.js
@@ -1,7 +1,7 @@
 export default function() {
   this.transition(
-    this.hasClass('preview-test__overlay-wrapper'),
-    this.use('fade', { duration: 3000 }),
-    this.reverse('fade', { duration: 3000 })
+    this.toRoute('chapter'),
+    this.use('toUp'),
+    this.reverse('toDown')
   );
 }
