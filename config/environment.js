9a8c95e87d91f12d99859349d450909dfdc4e45d
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2016-01-12 11:37:16 +0100

    fixes enviroment

--- config/environment.js
+++ config/environment.js
@@ -26,7 +26,7 @@ module.exports = function(environment) {
       'style-src': "'self' 'unsafe-inline' fonts.googleapis.com",
       'script-src': "'self' 'unsafe-eval' 'unsafe-inline' " +
         "apis.google.com *.firebaseio.com",
-      'connect-src': "'self' *.mapbox.com http://localhost:5000" +
+      'connect-src': "'self' *.mapbox.com http://localhost:5000 " +
       "ws://localhost:5000 wss://s-dal5-nss-15.firebaseio.com",
     },
 
