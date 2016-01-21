82f91986c84de7a391276fc2ed3e4fc50b4bb2a2
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2016-01-21 11:36:49 +0100

    add admin-options component

--- app/components/admin-options.js
+++ app/components/admin-options.js
@@ -1,9 +1,70 @@
 import Ember from 'ember';
 
 const {
-  Component
+  Component,
+  inject,
+  run: {
+    bind
+  }
 } = Ember;
 
 export default Component.extend({
-  classNames: ['admin-options']
+  classNames: ['admin-options'],
+
+  // Targeting service
+  targeting: inject.service('targeting'),
+
+  // Variables
+  adminText: '',
+
+  didInsertElement: function () {
+    $(document).on('click', bind(this, this.addRouteSegment) );
+  },
+
+  addRouteSegment: function (e) {
+    if(e.altKey) {
+      let newRouetPoint = this.get('targeting.map').unproject([e.clientX, e.clientY]);
+      if(!this.get('targeting.currentChapter.route')) {
+        this.get('targeting.currentChapter').set('route', []);
+      }
+      this.get('targeting.currentChapter.route').pushObject(newRouetPoint);
+      this.get('targeting.currentChapter').save();
+    }
+  },
+
+  actions: {
+    save: function () {
+      let center = this.get('targeting.map').getCenter(),
+        zoom = this.get('targeting.map').getZoom(),
+        bearing = this.get('targeting.map').getBearing(),
+        pitch = this.get('targeting.map').getPitch();
+
+      this.get('targeting.currentChapter').set('lat', center.lat);
+      this.get('targeting.currentChapter').set('lng', center.lng);
+      this.get('targeting.currentChapter').set('bearing', bearing);
+      this.get('targeting.currentChapter').set('pitch', pitch);
+      this.get('targeting.currentChapter').set('zoom', zoom);
+      this.get('targeting.currentChapter').save();
+      this.set('adminText', 'Chapter saved: ' + this.get('targeting.currentChapter.number'));
+    },
+
+    removeRoute: function () {
+      this.get('targeting.currentChapter').set('route', null);
+      this.get('targeting.currentChapter').save();
+      this.set('adminText', 'Route deleted: ' + this.get('targeting.currentChapter.number'));
+    },
+
+    addHotspot: function () {
+      console.log('add Hotspot');
+      this.set('adminText', 'Hotspot added');
+    },
+
+    toggleChapter: function () {
+      this.get('targeting').toggleProperty('isVisible');
+    },
+
+    toggleAnimation: function () {
+      this.get('targeting').toggleProperty('mapAnimation');
+    }
+  }
 });
