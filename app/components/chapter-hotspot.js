1abe97956e3622518c7ca9026adb472f9317647c
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2016-01-22 12:19:32 +0100

    design chapter-hotspot

--- app/components/chapter-hotspot.js
+++ app/components/chapter-hotspot.js
@@ -2,14 +2,31 @@ import Ember from 'ember';
 import Gestures from 'ember-cli-tuio/mixins/gestures';
 
 const {
-  Component
+  Component,
+  computed
 } = Ember;
 
 export default Component.extend(Gestures, {
   classNames: ['chapter-hotspot'],
+  classNameBindings: ['modifierType', 'modifierNumber', 'modifierOpen'],
+
+  // BEM modifier
+  modifierNumber: computed('number', function () {
+    return 'chapter-hotspot--' + this.get('number');
+  }),
+
+  modifierType: computed('hotspot.type', function () {
+    return 'chapter-hotspot--' + this.get('hotspot.type');
+  }),
+
+  modifierOpen: computed('open', function () {
+    if(this.get('open')) {
+      return 'chapter-hotspot--open';
+    }
+  }),
 
   // Gesture Settings
-  gestures: ['tap'],
+  gestures: ['tap', 'pinch', 'pinchin'],
 
   recognizers: {
     pan: {direction: Hammer.DIRECTION_HORIZONTAL}
@@ -18,12 +35,16 @@ export default Component.extend(Gestures, {
   // Video.js settings
   setup: {
     controls: true,
-    fluid: true
+    fluid: true,
+  },
+
+  pinchin: function () {
+    this.set('open', false);
   },
 
   actions: {
     toggleHotspot: function () {
-      this.$().parent().find('.chapter-hotspot__overlay').slideToggle(300);
+      this.toggleProperty('open');
     }
   }
 });
